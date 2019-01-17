177fc161ec77ada0f79fc015e59c6d7985796081
cactus36 <nicolasgauthier@icloud.com>
2019-01-17 14:43:23 +0100

    Classe jointe

--- Projet_e_commerce/src/com/intiformation/ecommerce/entity/LigneCommande.java
+++ Projet_e_commerce/src/com/intiformation/ecommerce/entity/LigneCommande.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -18,6 +22,18 @@ import javax.persistence.Table;
 //@Inheritance(strategy=InheritanceType.JOINED)
 public class LigneCommande implements Serializable{
 
+	//champs de création de la table 
+	@EmbeddedId
+	private LigneCommandeProduit id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("idProduit")
+    private Produit produit;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("idCommande")
+    private Commande commande;
+	
 	//champs
 	@Column(name="quantite")
 	private int quantite;
@@ -28,8 +44,22 @@ public class LigneCommande implements Serializable{
 	@OneToOne(cascade = CascadeType.ALL)
 	private GestionPanier panier;
 
-	public LigneCommande(int quantite, int prix, GestionPanier panier) {
+	//ctor
+	public LigneCommande(LigneCommandeProduit id, Produit produit, Commande commande, int quantite, int prix,
+			GestionPanier panier) {
 		super();
+		this.id = id;
+		this.produit = produit;
+		this.commande = commande;
+		this.quantite = quantite;
+		this.prix = prix;
+		this.panier = panier;
+	}
+
+	public LigneCommande(Produit produit, Commande commande, int quantite, int prix, GestionPanier panier) {
+		super();
+		this.produit = produit;
+		this.commande = commande;
 		this.quantite = quantite;
 		this.prix = prix;
 		this.panier = panier;
@@ -38,6 +68,57 @@ public class LigneCommande implements Serializable{
 	public LigneCommande() {
 		super();
 	}
+
+	//getter et setter
+	public LigneCommandeProduit getId() {
+		return id;
+	}
+
+	public void setId(LigneCommandeProduit id) {
+		this.id = id;
+	}
+
+	public Produit getProduit() {
+		return produit;
+	}
+
+	public void setProduit(Produit produit) {
+		this.produit = produit;
+	}
+
+	public Commande getCommande() {
+		return commande;
+	}
+
+	public void setCommande(Commande commande) {
+		this.commande = commande;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public GestionPanier getPanier() {
+		return panier;
+	}
+
+	public void setPanier(GestionPanier panier) {
+		this.panier = panier;
+	}
+
+	
 	
 	
 }
