14f481477827111a35c161bf38ce000f479e92d0
cactus36 <nicolasgauthier@icloud.com>
2019-01-17 15:09:03 +0100

    package de test

---
+++ Projet_e_commerce/src/com/intiformation/ecommerce/test/AppTestEntity.java
@@ -0,0 +1,14 @@
+package com.intiformation.ecommerce.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppTestEntity {
+
+	public static void main(String[] args) {
+		// context
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+	}
+
+}
